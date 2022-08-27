8aa5ae195b09a969d5a777b76bc85d5f8acea479
Chiang <fuchiang137@gmail.com>
2022-08-27 13:09:33 +0800

    Added device selector for better portability

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+import torch
 import torch.nn as nn
 import torchvision.models as models
 from torch.utils.data import DataLoader
 import time
 from img_loader import img_loader
-
 import csv
+
+device = torch.device("mps")
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    model = models.alexnet(pretrained=True).eval().to("cuda")
+    model = models.alexnet(pretrained=True).eval().to(device)
     model.classifier = nn.Sequential(*[model.classifier[i] for i in range(6)])
 
     start = time.time()
-    datasetReader = img_loader(r'C:\Users\fuchi\Desktop\Buffer')
+    datasetReader = img_loader(r'/Users/fuchiang137/Downloads/Buffer')
     dataloader = DataLoader(datasetReader, batch_size=512, shuffle=False, pin_memory=True, num_workers=12)
 
     num_vec = 0
@@ -20,7 +23,7 @@ if __name__ == '__main__':
     detections_batch = []
     for i, (batch) in enumerate(dataloader):
         num_vec += len(batch[1])
-        data_for_process = batch[0].to("cuda")
+        data_for_process = batch[0].to(device)
         print(data_for_process.shape)
         result = model(data_for_process).detach().cpu().numpy()
         detections_batch.append([result, batch[1]])
@@ -31,7 +34,7 @@ if __name__ == '__main__':
     vec_header = ['filename']
     [vec_header.append(f'vec_{i}') for i in range(dim_vec)]
 
-    with open(rf'C:\Users\fuchi\Desktop\image_vec_n_{num_vec}_d_{dim_vec}.csv', 'w', encoding='utf-8') as file:
+    with open(rf'/Users/fuchiang137/Downloads/image_vec_n_{num_vec}_d_{dim_vec}.csv', 'w', encoding='utf-8') as file:
         writer = csv.writer(file, delimiter=' ')
         writer.writerow([num_vec, dim_vec])
         writer.writerow(vec_header)
